4dbcd878243bcf47aa4fd615c10efc2d341f40e6
victorkuoz <b08902069@csie.ntu.edu.tw>
2023-04-16 00:51:00 +0800

    task1

--- assignment2/climber.c
+++ assignment2/climber.c
@@ -10,9 +10,81 @@
 #include "climber.h"
 #include "Wall.h"
 
+typedef struct rock Rock;
+
 struct path findShortestPath(Wall w, int reach, Colour colour) {
     // TODO - Task 1
     struct path p = {0, NULL};
+
+    int height = WallHeight(w);
+    int width = WallWidth(w);
+
+    Rock *rocks = calloc(height * width, sizeof(Rock));
+    int numOfRocks = WallGetAllRocks(w, rocks);
+
+    bool **explored = calloc(height, sizeof(bool*));
+    for (int r = 0; r < height; r++)
+        explored[r] = calloc(width, sizeof(bool));
+
+    Rock ***prevRock = calloc(height, sizeof(Rock**));
+    for (int r = 0; r < height; r++)
+        prevRock[r] = calloc(width, sizeof(Rock*));
+
+    int bfsSize = 0;
+    Rock **bfs = calloc(height * width, sizeof(Rock*));
+    for (int i = 0; i < numOfRocks; i++) {
+        if (rocks[i].row <= reach && rocks[i].colour == colour) {
+            bfs[bfsSize++] = &rocks[i];
+            explored[rocks[i].row][rocks[i].col] = true;
+        }
+    }
+
+    int bfsIdx = 0, squ = reach * reach;
+    while (bfsIdx < bfsSize) {
+        Rock *cur = bfs[bfsIdx++];
+
+        if (height <= cur->row + reach) {
+            Rock **revPath = calloc(height * width, sizeof(Rock));
+            while (cur) {
+                revPath[p.numRocks++] = cur;
+                cur = prevRock[cur->row][cur->col];
+            }
+
+            p.rocks = calloc(p.numRocks, sizeof(Rock));
+            for (int i = 0, j = p.numRocks - 1; i < p.numRocks; i++, j--)
+                p.rocks[i] = *revPath[j];
+            free(revPath);
+            break;
+        }
+
+        for (int i = 0; i < numOfRocks; i++) {
+            int r = rocks[i].row, c = rocks[i].col;
+            if (rocks[i].colour != colour || explored[r][c])
+                continue;
+
+            int dr = abs(r - cur->row), dc = abs(c - cur->col);
+
+            if (squ < dr*dr + dc*dc)
+                continue;
+
+            bfs[bfsSize++] = &rocks[i];
+            explored[r][c] = true;
+            prevRock[r][c] = cur;
+        }
+    }
+
+    free(rocks);
+
+    for (int r = 0; r < height; r++)
+        free(explored[r]);
+    free(explored);
+
+    for (int r = 0; r < height; r++)
+        free(prevRock[r]);
+    free(prevRock);
+
+    free(bfs);
+
     return p;
 }
 
